c79f8ccbca14c3a903b7b4a2c7b223adae965f8b
Huzaif Naeem <huzaifnaeem30@gmail.com>
2024-05-14 01:04:22 +0500

    update js file

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-#! /usr/bin/env node
+// A simple currency converter created with the help of typescript, node.js and inquirer.
+// import inquirer from npm library.
 import inquirer from "inquirer";
+//
 const currency = {
     USD: 1, //base currency
     EUR: 0.94,
-    GBP: 0.80,
+    GBP: 0.8,
     INR: 83.61,
-    PKR: 277.30,
+    PKR: 277.3,
 };
 let answer = await inquirer.prompt([
     {
         name: "from",
         message: "Enter from currency",
         type: "list",
-        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
     },
     {
         name: "to",
         message: "Enter to currency",
         type: "list",
-        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
     },
     {
         name: "amount",
         message: "Enter your amount",
-        type: "number"
-    }
+        type: "number",
+    },
 ]);
+// Convert currency from one unit to another based on provided amounts and conversion rates.
 let fromAmount = currency[answer.from];
 let toAmount = currency[answer.to];
 let amount = answer.amount;
